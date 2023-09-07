b2ad2a52d0f1d6fb953ca3636b88b2cb88f2b3da
jdrf0722 <137022903+jdrf0722@users.noreply.github.com>
2023-09-07 00:30:02 -0600

    Feature update task  (#2)
    * se cambian estilos
    
    * añade estilos
    
    * crea update component
    
    * actualiza coomponentes
    
    * avance componente
    
    * mod
    
    * avance componente
    
    * se actualiza el context
    
    * se actualiza el context
    
    * se actualiza el context
    
    * actualizacion de estilos

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,21 +1,28 @@
 import { TaskContext } from "../context/TaskContext.jsx";
+import TaskUpdate from './TaskUpdate.jsx'
 import { useContext } from "react";
 
 function TaskCard({ task }) {
   const { deleteTask } = useContext(TaskContext);
 
   return (
-    <div className="bg-gray-800 text-white p-4 rounded-md">
-      <h1 className="text-xl font-bold capitalize"> {task.title} </h1>
-      <p className="text-gray-500 text-sm">{task.description} </p>
-      <button
-        className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400"
-        onClick={() => {
-          deleteTask(task.id);
-        }}
-      >
-        Eliminar
-      </button>
+    <div className="bg-gray-800 text-white p-3 md:p-6 rounded-md ">
+      <div className="overflow-hidden">
+        <p className="text-xl font-bold capitalize mb-2">{task.title}</p>
+        <p className="text-gray-500 text-sm">{task.description} </p>
+      </div>
+      <div className="">
+        <TaskUpdate key={task.id} taskId={task.id}/>
+        <button
+          className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400 w-full block"
+          onClick={() => {
+            deleteTask(task.id);
+          }}
+        >
+          Eliminar
+        </button>
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/TaskUpdate.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { TaskContext } from "../context/TaskContext.jsx";
+import { useContext } from "react";
+
+function Form({ taskId, showInputs }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const { updateTask } = useContext(TaskContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateTask({
+      taskId,
+      title,
+      description,
+    });
+    setTitle("");
+    setDescription("");
+    showInputs(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Update your task title"
+        className="p-2 mb-3 mt-4 rounded-md bg-slate-200 w-full text-center text-black"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <textarea
+        placeholder="Type your task desciption"
+        className=" block mb-4 rounded-md text-center resize-none pt-2 md:pt-5 bg-slate-200 w-full text-black"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+      <button
+        type="submit"
+        className="bg-blue-500 px-2 py-1 rounded-md mt-4 hover:bg-blue-400 w-full block"
+      >
+        Save
+      </button>
+    </form>
+  );
+}
+
+function TaskUpdate({ taskId }) {
+  const [showInputs, setShowInputs] = useState(false);
+
+  const Update = () => (
+    <button
+      className="bg-blue-500 px-2 py-1 rounded-md mt-1.5 sm:mt-4 hover:bg-blue-400 w-full block"
+      onClick={() => {
+        setShowInputs(true);
+      }}
+    >
+      Update
+    </button>
+  );
+
+  return (
+    <>
+      {showInputs ? (
+        <Form showInputs={setShowInputs} taskId={taskId} />
+      ) : (
+        <Update />
+      )}
+    </>
+  );
+}
+
+export default TaskUpdate;
